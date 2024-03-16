f570dd319317e1282837d29fc70fdbfe13c14024
dpchamps <me@davecampion.com>
2024-03-15 22:29:07 -0700

    more bugs

--- src/utilities/data-fetcher.ts
+++ src/utilities/data-fetcher.ts
@@ -11,10 +11,13 @@ export const createDataSynchronizer = (ms: number, questionsPath: string) => {
     let then = Date.now();
     let data: null|Record<string, unknown> = null;
 
-    process.on("SIGCONT", async () => {
-        console.log(`Received SIGCONT, updating data`);
-        data = await readDataFromDisk(questionsPath);
-    });
+    if(typeof process !== "undefined"){
+        process.on("SIGCONT", async () => {
+            console.log(`Received SIGCONT, updating data`);
+            data = await readDataFromDisk(questionsPath);
+        });
+    }
+
 
 
     return async () => {
