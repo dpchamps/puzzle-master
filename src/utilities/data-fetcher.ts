d2e1ff7083e614d0b8900e76a4b08332b06468cd
dpchamps <me@davecampion.com>
2024-03-15 22:25:04 -0700

    bugs ahoy

--- src/utilities/data-fetcher.ts
+++ src/utilities/data-fetcher.ts
@@ -1,5 +1,4 @@
 import fs from "fs/promises";
-import path from "path";
 import assert from "assert";
 
 const readDataFromDisk = async (path: string) => {
@@ -9,21 +8,20 @@ const readDataFromDisk = async (path: string) => {
 }
 export const createDataSynchronizer = (ms: number) => {
     const questionsPath = process.env.QUESTIONS_PATH || "";
-    const resolvedQuestionsPath = path.resolve(questionsPath);
 
     let then = Date.now();
     let data: null|Record<string, unknown> = null;
 
     process.on("SIGCONT", async () => {
         console.log(`Received SIGCONT, updating data`);
-        data = await readDataFromDisk(resolvedQuestionsPath);
+        data = await readDataFromDisk(questionsPath);
     });
 
 
     return async () => {
         const now = Date.now();
         if(data === null || (now - then) > ms) {
-            data = await readDataFromDisk(resolvedQuestionsPath);
+            data = await readDataFromDisk(questionsPath);
             then = now;
         }
         assert(data !== null);
